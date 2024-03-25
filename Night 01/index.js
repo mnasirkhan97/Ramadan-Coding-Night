aec4475c1cd7c96ebaa3e2b9dcc7c2811566fb9e
M Nasir Khan <immnk97@gmail.com>
2024-03-25 12:57:13 +0500

    Night01

---
+++ Night 01/index.js
@@ -0,0 +1,8 @@
+// console.log("Nasir Khan");
+// console.log(2 + 3);
+// var userName = "Nasir Khan";
+// console.log(userName)
+var num1 = 10;
+var num2 = 20;
+var result = num1 + num2;
+console.log(result);
---
+++ Night 01/index.ts
@@ -0,0 +1,15 @@
+// console.log("Nasir Khan");
+
+
+// console.log(2 + 3);
+
+// var userName = "Nasir Khan";
+
+// console.log(userName)
+
+let num1 = 10;
+
+let num2 = 20;
+
+let result = num1 + num2;
+console.log(result)
