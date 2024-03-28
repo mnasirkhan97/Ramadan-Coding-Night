e2a486a67d880b3a863d62d70912e70ff954fb1e
M Nasir Khan <immnk97@gmail.com>
2024-03-28 18:33:43 +0500

    Night 02

---
+++ Night 02/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { messege: "Enter first number", type: "number", name: "firstNumber" },
+    { messege: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        messege: "select one of the operators to perform operation ",
+        type: "list",
+        name: "oprator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement
+if (answer.oprator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.oprator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.oprator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.oprator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid operators");
+}
+// console.log(answer);
